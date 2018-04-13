e1e6f1ea327a2f20f390148e9ff57ebb09fcbfb1
oskrfabian <oskrfabian.system@gmail.com>
2018-04-13 14:29:37 -0600

    codigo de alarma

--- Parcial-practico-2_arduino/Parcial-practico-2_arduino.ino
+++ Parcial-practico-2_arduino/Parcial-practico-2_arduino.ino
@@ -3,10 +3,16 @@ int toneVal;
 
 void setup(){
      pinMode(8, OUTPUT); // Definimos el pin 8 como salida.
+     Serial.begin(9600);
 }
-
+// para zumador  
 void loop(){
+//mandar una mensaje 
+
+  
+  // el codigo la alarma 
      for(int x=0; x<180; x++){
+      Serial.println("alarma ");
             // convertimos grados en radianes para luego obtener el valor.
             sinVal = (sin(x*(3.1412/180)));
             // generar una frecuencia a partir del valor sin
