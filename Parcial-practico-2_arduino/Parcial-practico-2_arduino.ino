273f074cfb208bbd770da5f54e4f09b65dd34e21
oskrfabian <oskrfabian.system@gmail.com>
2018-04-13 14:15:08 -0600

    modificacion de codigo de arduino

--- Parcial-practico-2_arduino/Parcial-practico-2_arduino.ino
+++ Parcial-practico-2_arduino/Parcial-practico-2_arduino.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+float sinVal;
+int toneVal;
 
+void setup(){
+     pinMode(8, OUTPUT); // Definimos el pin 8 como salida.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+     for(int x=0; x<180; x++){
+            // convertimos grados en radianes para luego obtener el valor.
+            sinVal = (sin(x*(3.1412/180)));
+            // generar una frecuencia a partir del valor sin
+            toneVal = 2000+(int(sinVal*1000));
+            tone(8, toneVal);
+            delay(2); 
+     }   
 }
